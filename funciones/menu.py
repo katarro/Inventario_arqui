c722c347a7c9269c902038d3a522aa41c535a407
frankbydefault <franco.hauva@gmail.com>
2023-05-26 14:04:54 -0400

    login service

--- funciones/menu.py
+++ funciones/menu.py
@@ -1,4 +1,5 @@
 import funciones.funcion_registrar_usuario as Funciones
+import funciones.funcion_inicio_sesion as InicioSesion
 
 def menu():
     opcion = -1
@@ -6,8 +7,11 @@ def menu():
         print("Bienvenido al sistema de inventario")
         print("Elige una opcion: ")
         print("\t1 - Registrar usuario")
-        print("\t2 - Crear catalogo")
-        print("\t3 - Salir")
+        print("\t2 - Iniciar sesion")
+        print("\t3 - Crear catalogo")
+        print("\t4 - Salir")
         opcion = int(input("Opcion: "))
         if opcion == 1:
             Funciones.funcion_registrar_usuario()
+        elif opcion == 2:
+            InicioSesion.funcion_inicio_sesion()
